973a73456596e812bd146887d9fef8ff01be2aca
jonrjohnson34 <jr2034@gmail.com>
2016-09-28 14:25:29 -0700

    started working on the award page

--- client/awards/awards.js
+++ client/awards/awards.js
@@ -1,7 +1,29 @@
-angular.module("collegepool").directive('awards', function() {
+angular.module("collegepool").directive('awards', function () {
   return {
     restrict: 'E',
     templateUrl: 'client/awards/awards.html',
-    controllerAs: 'awards'
+    controllerAs: 'awards',
+    controller: function ($scope, $reactive, $q) {
+
+      $reactive(this).attach($scope);
+
+      var getData = () => {
+        var deferred = $q.defer();
+        Meteor.call('getTotals', (error, result) => {
+          if (error) {
+            deferred.reject('error');
+          } else {
+            deferred.resolve(result);
+          }
+        });
+        return deferred.promise;
+      };
+
+      getData().then((data) => {
+        this.getAwards = data;
+        console.log(data);
+      });
+
+    }
   }
 });
